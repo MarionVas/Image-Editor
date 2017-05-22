e6a1917d8d6884cf371c1fc9e9dd75dd148ab3c8
vasant42 <MarionVas@users.noreply.github.com>
2017-05-22 00:44:58 +0000

    Initial commit

---
+++ image_processing.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/*
+ * Read an image from the standard input and set the red value of each pixel to
+ * zero.
+ */
+void remove_red()
+{
+   printf("remove_red""\n");
+}
+
+/*
+ * Read an image from the standard input and convert it from colour to black and white.
+ */
+void convert_to_black_and_white()
+{
+  printf("convert_to_black_and_white\n");
+}
+
+/*
+ * Read an image from the standard input and convert it to a square image.
+ */
+void instagram_square()
+{
+ 	printf("instagram_square\n");
+}
+
