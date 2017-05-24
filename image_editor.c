89cbe435d3e1730a12e3d74f91ac7bd8c2c9f6e5
vasant42 <MarionVas@users.noreply.github.com>
2017-05-24 18:07:54 +0000

    Changed error out put from stdout to stderr

--- image_editor.c
+++ image_editor.c
@@ -17,20 +17,25 @@ int main(int argc, char *argv[]) {
 
 		if (argc != 2)
 		{
-			printf("Error: Expecting one command-line argument, which needs to be either 1, 2, or 3.\n");
+			fprintf(stderr, "Error: Expecting one command-line argument, which needs to be either 1, 2, or 3.\n");
 		}
-
-		if (*argv[1] == '1')
-		{
-			remove_red();
-		}
-		else if (*argv[1] == '2')
-		{
-			convert_to_black_and_white();
-		}
-		else if (*argv[1] == '3')
-		{
-			instagram_square();
+		else {
+			if (*argv[1] == '1')
+			{
+				remove_red();
+			}
+			else if (*argv[1] == '2')
+			{
+				convert_to_black_and_white();
+			}
+			else if (*argv[1] == '3')
+			{
+				instagram_square();
+			}
+			else
+			{
+				fprintf(stderr, "Error: Expecting one command-line argument, which needs to be either 1, 2, or 3.\n");	
+			}
 		}
         return 0;
 }
